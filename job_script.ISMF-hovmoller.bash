52d9a9f2fba6c01169633c90f31cdc0980f01e2c
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-09-13 08:18:36 -0700

    Add config files and job scripts for hovmoller plots

---
+++ job_script.ISMF-hovmoller.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH --partition=regular
+#SBATCH --qos=premium
+#SBATCH -C haswell
+#SBATCH --nodes=1
+#SBATCH --time=4:00:00
+#SBATCH --account=e3sm
+#SBATCH --job-name=mpas_analysis
+#SBATCH --output=mpas_analysis.o%j
+#SBATCH --error=mpas_analysis.e%j
+#SBATCH -L cscratch1,SCRATCH,project
+
+export OMP_NUM_THREADS=1
+
+source /global/cfs/cdirs/e3sm/software/anaconda_envs/load_test_e3sm_unified.sh
+export HDF5_USE_FILE_LOCKING=FALSE
+
+srun -N 1 -n 1 python -m mpas_analysis configs/polarRegions.conf B-ISMF-hovmoller.cfg
+
