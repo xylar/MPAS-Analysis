9116302f4b487f31da8ec66c9c405846fc581e01
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-11-19 04:49:55 -0800

    Move config and job files to separate dirs
    Update the tasks to generate
    
    Add yrs 21-50

--- yrs21-50/job_script.ISMF_vs_MGM.bash
+++ yrs21-50/job_script.ISMF_vs_MGM.bash
@@ -4,17 +4,18 @@
 #SBATCH --qos=premium
 #SBATCH -C haswell
 #SBATCH --nodes=1
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 #SBATCH --account=e3sm
 #SBATCH --job-name=mpas_analysis
-#SBATCH --output=mpas_analysis_mgm.o%j
-#SBATCH --error=mpas_analysis_mgm.e%j
+#SBATCH --output=mpas_analysis.o%j
+#SBATCH --error=mpas_analysis.e%j
 #SBATCH -L cscratch1,SCRATCH,project
 
 export OMP_NUM_THREADS=1
 
-source /global/cfs/cdirs/e3sm/software/anaconda_envs/load_test_e3sm_unified.sh
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate mpas-analysis
 export HDF5_USE_FILE_LOCKING=FALSE
 
-srun -N 1 -n 1 python -m mpas_analysis configs/polarRegions.conf B-MGM.cfg
+srun -N 1 -n 1 python -m mpas_analysis configs/polarRegions.conf yrs21-50/B-ISMF_vs_B-MGM.cfg
 
