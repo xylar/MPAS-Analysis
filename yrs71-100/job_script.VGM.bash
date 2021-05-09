a0e778d057704bc39ab56b4aebe695c2e16e0a99
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-05-09 00:11:53 -0700

    Add config files and job scripts for yrs 71-100

---
+++ yrs71-100/job_script.VGM.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH --qos=regular
+#SBATCH -C haswell
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --account=m3412
+#SBATCH --job-name=mpas_analysis
+#SBATCH --output=mpas_analysis_vgm.o%j
+#SBATCH --error=mpas_analysis_vgm.e%j
+#SBATCH -L cscratch1,SCRATCH,project
+
+export OMP_NUM_THREADS=1
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate mpas_dev
+export HDF5_USE_FILE_LOCKING=FALSE
+
+srun -N 1 -n 1 mpas_analysis configs/polarRegions.conf yrs71-100/VGM.cfg
+
