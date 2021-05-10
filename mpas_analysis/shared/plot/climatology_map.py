ef8cde9b563d3e1e8acd112e6e32f90647514ec6
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-05-09 23:15:44 -0700

    Adjust cartopy grid lines

--- mpas_analysis/shared/plot/climatology_map.py
+++ mpas_analysis/shared/plot/climatology_map.py
@@ -527,12 +527,14 @@ def plot_polar_projection_comparison(
 
         gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), color='k',
                           linestyle=':', zorder=5, draw_labels=True)
-        gl.xlocator = mticker.FixedLocator(np.arange(-180., 181., 60.))
+        gl.xlocator = mticker.FixedLocator(np.arange(-180., 181., 30.))
         gl.ylocator = mticker.FixedLocator(np.arange(-80., 81., 10.))
         gl.n_steps = 100
         gl.right_labels = False
+        gl.left_labels = False
         gl.xformatter = cartopy.mpl.gridliner.LONGITUDE_FORMATTER
         gl.yformatter = cartopy.mpl.gridliner.LATITUDE_FORMATTER
+        gl.xlabel_style['size'] = 14
 
         if levels is None:
             plotHandle = ax.pcolormesh(x, y, array, cmap=colormap, norm=norm,
